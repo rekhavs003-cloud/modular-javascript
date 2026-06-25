6df32bbcbf8ee778db5b469bf34cef4b86dcd0a9
Rekha <rekhavs003@gmail.com>
2026-06-25 00:07:56 -0500

    assignment completed

--- tutorials/02-modules/commonjs-modules/exercises/demo/gameData.js
+++ tutorials/02-modules/commonjs-modules/exercises/demo/gameData.js
@@ -2,3 +2,4 @@ const startingHealthValue = 100;
 const defaultWeaponName = "Wooden Training Sword";
 
 // DEMO: Create an exports object to bundle all assets to be exported
+module.exports ={startingHealthValue,defaultWeaponName};
--- tutorials/02-modules/commonjs-modules/exercises/demo/main.js
+++ tutorials/02-modules/commonjs-modules/exercises/demo/main.js
@@ -2,10 +2,11 @@
 
 // DEMO: Import the assets from gameData.js
 
+const gameData = require ("./gameData");
 // DEMO: Replace null values below with the correct references to the imported assets
 console.log("--- INITIALIZING ARCADE ASSETS ---");
-console.log(`Configuring system... Default Player Health: ${null}`);
-console.log(`Spawning equipment... Starter weapon designated: ${null}`);
+console.log(`Configuring system... Default Player Health: ${gameData.startingHealthValue}`);
+console.log(`Spawning equipment... Starter weapon designated: ${gameData.defaultWeaponName}`);
 
 /* 
     Follow up with additional exercises after demo for hands-on practice 
